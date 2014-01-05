fd748fa780ca6be9c48e31e2133167bda6b8b48b
Antonio Chavez <cavjzz@gmail.com>
2014-01-04 22:38:21 -0600

    Added combinations

--- lib/game/grid.rb
+++ lib/game/grid.rb
@@ -1,6 +1,6 @@
 module Game
   class Grid
-    attr_reader :field
+    attr_reader :field, :rows
 
     def initialize
       @field = {
@@ -8,10 +8,27 @@ module Game
         b1: nil, b2: nil, b3: nil,
         c1: nil, c2: nil, c3: nil
       }
+
+      @rows = {
+        row1: [:a1, :a2, :a3],
+        row2: [:a1, :a2, :a3],
+        row3: [:a1, :a2, :a3],
+        row4: [:a1, :a2, :a3],
+        row5: [:a1, :a2, :a3],
+        row6: [:a1, :a2, :a3],
+        row7: [:a1, :a2, :a3],
+        row8: [:a1, :a2, :a3]
+      }
     end
 
     def free_rows
-      Array.new(8)
+      combinations = []
+      @rows.each do |key, value|
+        if @field[value[0]].nil?  and @field[value[1]].nil?  and @field[value[2]].nil?
+          combinations << key
+        end
+      end
+      combinations
     end
 
   end
