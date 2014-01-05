1af3c8df988abd2e21251ed40c06d45f9aaa8935
Antonio Chavez <cavjzz@gmail.com>
2014-01-04 22:05:40 -0600

    Started to create the environment class

--- lib/game/environment.rb
+++ lib/game/environment.rb
@@ -1,5 +1,19 @@
 module Game
   class Environment
+    attr_reader :shape_type
+    def initialize(shape_type)
+      @shape_type = shape_type
+    end
 
+    def computer_shape_type
+      case @shape_type
+      when :cross
+        return :nought
+      when :nought
+        return :cross
+      else
+        raise Exception, "did you forget to assign the type first?"
+      end
+    end
   end
 end
--- spec/lib/game/environment_spec.rb
+++ spec/lib/game/environment_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Game::Environment do
 
+  let!(:game) { Game::Environment.new(:nought) }
+
+  it 'accepts the type shape type' do
+    expect(game.shape_type).to eq(:nought)
+  end
+
+  it 'assigns the opposite shape type to the computer' do
+    expect(game.computer_shape_type).to eq(:cross)
+  end
+
 end
